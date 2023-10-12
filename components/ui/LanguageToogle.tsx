04128c0f753861660c23b264f80a4ce7f6f1717f
bogdan shulika <bsshul@gmail.com>
2023-10-13 00:11:01 +0300

    Fix ts

--- components/ui/LanguageToogle.tsx
+++ components/ui/LanguageToogle.tsx
@@ -4,6 +4,7 @@ import { languages } from "@/public/lang";
 import { CurrentLanguage } from "@/context/LangContext";
 
 export default function LanguageToogle() {
+  // @ts-ignore: Unreachable code error
   const { changeLanguages } = CurrentLanguage();
   const switchToUA = () => changeLanguages(languages[0]);
   const switchToRUS = () => changeLanguages(languages[1]);
