7af5a8812c4841d57bf2596bc0d7a2c3f42df10b
bogdan shulika <bsshul@gmail.com>
2023-10-28 12:44:01 +0300

    Button

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,11 @@ export default function Home() {
       <ZoomImage />
       <Swiper_products slides={products} />
       <TabsElement />
+      <div className="wrapper flex flex-col">
+        <button
+          className={`w-24 h-10 rounded-lg bg-white text-yellow-600 transition-colors border-2 border-black relative font-medium overflow-hidden before:content-["Hover"] before:absolute before:top-1/2 before:-translate-y-1/2 before:left-1/2 before:-translate-x-1/2 before:z-20 hover:before:text-black after:transition-all after:absolute after:bg-yellow-400 after:w-full after:h-full after:top-0 after:left-0 after:z-10 after:-translate-y-full hover:after:translate-y-0`}
+        />
+      </div>
     </>
   )
 }
